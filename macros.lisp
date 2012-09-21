0b091f6536dc8feee28c5a609a10c78ec49de66e
Vishal Jain <cse.vishal@gmail.com>
2012-09-21 23:43:45 +0530

    from onlisp.pdf chapter 7 section 7.1 7.2 .. nice

--- macros.lisp
+++ macros.lisp
@@ -1,6 +1,52 @@
+(eql (list 'a 'b 'c) `(a b c)) ; NIL
+(list 'a 'b 'c) ; (A B C)
+`(a b c) ; (A B C) 
+'('a 'b 'c); ('A 'B 'C)
+(list ''a 'b 'c) ; ('A B C)
+(list '''a 'b 'c) ; (''A B C)
+
+(signum -2.5) ; -1.0
+(= (truncate (signum 2.5)) 1) ; TRUE
+(string= "vis" "vis") ; TRUE
+
+(eq 'A 'A) ; T
+(eql '('A) '('A)) ; NIL
+'('A 'B)
+
+;; splicing - comma-at
+(setq x '(1 2 3))
+`(4 ,@x); (4 1 2 3)
+`(4 ,x); (4 (1 2 3))
+(setq x 2)
+`(4 ,@x) ; (4 . 2)
+`(4 ,@x 10) ; ERROR
+
+(defmacro nil! (x)
+;  (list 'setq x nil))
+  `(setq ,x nil))
+
+(nil! x)
+(eql 'A 'a)
+(defmacro myeq (x y type)
+  `(case ,type
+     (':string (string= ,x ,y))
+     (':number (= ,x ,y))))
+
+(myeq "vis" "vis" :string) ;(CASE :STRING (':STRING (STRING= "vis" "vis")) (':NUMBER (= "vis" "vis")))
+(myeq 1 1 :number)
+
+(defmacro myeq2 (x y type)
+  (case type
+     (':string `(string= ,x ,y))
+     (':number `(= ,x ,y))))
+
+(myeq2 "vis" "vis" :string) ;(STRING= "vis" "vis")
+(myeq2 1 1 :number) ; (= 1 1)
+
+
 (if (< 2 3) "v1" "v2")
 ; C-x C-e ; eval expr (cursor at end of line)
-
+; C-c RET ; to check macro expansion (with cursor in macro call start brace)
 (if (< 4 3) "v1" '(1 2 3))
 (if (< 4 3) "v1")
 
@@ -27,3 +73,50 @@
   (dotimes (y 2)
     (format t "~a " (list x y)))
   (format t "~%"))
+
+; fibonacci series
+(do ((n 0 (+ 1 n))
+     (cur 0 next)
+     (next 1 (+ cur next)))
+    ((= n 10) cur)
+  (format t "~a " cur)
+;  (sleep 1)
+  (format t "~a~%" (+ cur 1)))
+  
+
+(find "vishal" "aeiou")
+
+(mod (sqrt 36.6))
+(zerop 0.1)
+
+(defun next-prime (number)
+  (do ((n number (1+ n)))
+      ((is-prime n) n)))
+
+(defun is-prime (number)
+  (when (> number 1)
+    (do ((n 2 (1+ n))
+	 (maxi (isqrt number)))
+	((or (> n maxi) (= 0 (mod number n))) 
+	 (not (= 0 (mod number n))))
+      (format t "~a ~a ~a ~%" n maxi (mod number n)))))
+;(> n (isqrt number)) t)
+
+(defun do-primes (start end)
+  (do ((lst nil)
+       (cur (next-prime start) (next-prime (+ cur 1))))
+      ((> cur end) (nreverse lst))
+    (push cur lst)))
+
+(do-primes 0 450)    
+(- 10 2 3)
+(is-prime 3)
+(isqrt 2)
+(not (= 0 (mod 10 2)))
+(not 1)
+(setf *ass* (list 1 2 3))
+(push 4 *ass*)
+
+(do ((var 0 (1+ var)))
+    ((> var 9)))
+
